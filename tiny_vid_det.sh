358a63f8c1c20f24e5430fd91dd786b87e4cdd97
ctmackay <charlestmackay@gmail.com>
2018-07-15 22:06:59 -0700

    add config files and gitignore

---
+++ tiny_vid_det.sh
@@ -0,0 +1,1 @@
+./darknet detector demo cfg/barberry.data cfg/tiny_barberry.cfg weights/tiny_barberry_final.weights $1 -thresh $2
---
+++ video_det.sh
@@ -0,0 +1,1 @@
+./darknet detector demo cfg/aic540.data cfg/aic540.cfg weights/aic540_final.weights $1 -thresh $2
